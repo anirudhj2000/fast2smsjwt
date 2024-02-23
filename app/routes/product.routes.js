aa3553f6c2491ff325f5560c4d3ce0fe04611000
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-02-23 09:30:05 +0530

    routes for products

---
+++ app/routes/product.routes.js
@@ -0,0 +1,48 @@
+const authJwt = require("../middleware/authJwt");
+const productController = require("../controllers/product.controller");
+
+module.exports = function (app) {
+  app.use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
+
+  app.get(
+    "/api/products",
+    [authJwt.verifyToken],
+    productController.getProducts
+  );
+
+  app.post(
+    "/api/products/search",
+    [authJwt.verifyToken],
+    productController.searchProducts
+  );
+
+  app.post(
+    "/api/products",
+    [authJwt.verifyToken],
+    productController.createProduct
+  );
+
+  app.put(
+    "/api/products/:id",
+    [authJwt.verifyToken],
+    productController.updateProduct
+  );
+
+  app.delete(
+    "/api/products/:id",
+    [authJwt.verifyToken],
+    productController.deleteProduct
+  );
+
+  app.get(
+    "/api/products/:id",
+    [authJwt.verifyToken],
+    productController.getProductById
+  );
+};
