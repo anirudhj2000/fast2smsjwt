d3cf5944e12d9125f0d134a2c0ae14f66c494d60
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-02-23 09:56:29 +0530

    user controller files

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,15 +1,76 @@
-exports.allAccess = (req, res) => {
-  res.status(200).send("Public Content.");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+exports.createUser = async (req, res) => {
+  try {
+    const { name, phoneNumber, city, address, state } = req.body;
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        phoneNumber,
+        city,
+        address,
+        state,
+      },
+    });
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
-exports.userBoard = (req, res) => {
-  res.status(200).send("User Content.");
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
-exports.adminBoard = (req, res) => {
-  res.status(200).send("Admin Content.");
+exports.getUserById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id },
+    });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
-exports.moderatorBoard = (req, res) => {
-  res.status(200).send("Moderator Content.");
+exports.updateUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const { name, phoneNumber, city, address, state } = req.body;
+    const updatedUser = await prisma.user.update({
+      where: { id },
+      data: {
+        name,
+        phoneNumber,
+        city,
+        address,
+        state,
+      },
+    });
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await prisma.user.delete({
+      where: { id },
+    });
+    res.status(204).end();
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
