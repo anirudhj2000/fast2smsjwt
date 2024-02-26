91167ed572d245e8b17c87767bf746ef2677844c
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-02-26 07:19:09 +0530

    indexjs changes

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ app.get("/", (req, res) => {
 require("./app/routes/auth.routes")(app);
 require("./app/routes/user.routes")(app);
 require("./app/routes/product.routes")(app);
+require("./app/routes/utils.routes")(app);
+require("./app/routes/cart.routes")(app);
+require("./app/routes/order.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
