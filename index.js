f03d3f7c8f4535c06e167a238dd2fbec3a6c39c5
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-03-12 00:21:11 +0530

    setting up for static files

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const path = require("path");
 
 const app = express();
 
 var corsOptions = {
-  origin: ["https://localhost:3001", "https://mahakali-ui-nuk9.vercel.app"],
+  origin: [
+    "https://localhost:3001",
+    "https://mahakali-ui-nuk9.vercel.app",
+    "http://144.24.111.231:3000",
+  ],
 };
 
 app.use(cors(corsOptions));
@@ -13,9 +18,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // simple route
-app.get("/", (req, res) => {
-  res.status(200).send({ message: "here here" });
-});
+// app.get("/", (req, res) => {
+//   res.status(200).send({ message: "here here" });
+// });
+
+app.use("/images", express.static("images"));
 
 require("./app/routes/auth.routes")(app);
 require("./app/routes/user.routes")(app);
