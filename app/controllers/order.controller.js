53d31496ae9d6b04262b8d70896bb7d170371a23
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-03-12 14:27:50 +0530

    email setup for orders

--- app/controllers/order.controller.js
+++ app/controllers/order.controller.js
@@ -1,11 +1,42 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "anirudhjoshi485@gmail.com",
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
 
 // Function to send email
 const sendOrderEmail = (order) => {
-  // Email sending logic goes here
-  // For demonstration purposes, we'll just log the order details
-  // console.log(`Sending order email for order ID: ${order.id}`);
+  // 2 Emails :  1 for customer , 1 for order
+  let subject =
+    "Order:" +
+    order.id +
+    " - " +
+    order.userDetails.name +
+    " - " +
+    order.userDetails.phoneNumber;
+
+  const mailOptions = {
+    from: "anirudhjoshi485@gmail.com",
+    to: "theinfinitysolutions9@gmail.com",
+    subject: subject,
+    html: "<div> <h2>Order Confirmation</h2> <p>Dear Anirudh,</p> <p>Thank you for your order. Your order details are as follows:</p> <h3>Order Summary</h3> <table> <tr > <th>Product</th> <th>Quantity</th> <th>Price</th> </tr> <!-- Products will be inserted here --> </table> <h3>User Details</h3> <p>City: Hyderabad</p> <p>Phone Number: 9398542806</p> <h3>Order Total</h3> <p>Total Amount: ₹2000</p> <p>Order Date: 2024-03-12</p> <p>Thank you for shopping with us. If you have any questions, please contact us at support@example.com.</p> <p>Best regards,</p> <p>Your Company Name</p> </div>",
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error("Error sending email: ", error);
+    } else {
+      console.log("Email sent: ", info.response);
+    }
+  });
 };
 
 exports.createOrder = async (req, res) => {
@@ -38,7 +69,7 @@ exports.createOrder = async (req, res) => {
     });
 
     // // Send email with order details
-    // sendOrderEmail(newOrder);
+    sendOrderEmail(newOrder);
 
     res.status(201).json(newOrder);
   } catch (error) {
