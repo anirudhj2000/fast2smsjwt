12019be90add61f32aa1ef5b65cf5128c97482f0
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-03-20 16:00:23 +0530

    main changes

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ require("./app/routes/product.routes")(app);
 require("./app/routes/utils.routes")(app);
 require("./app/routes/cart.routes")(app);
 require("./app/routes/order.routes")(app);
+require("./app/routes/category.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
